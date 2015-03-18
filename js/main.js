aa0ed30cbcb0b1d85bb6c2281dbe17ee245a9993
pkmoney1138 <pkmoney@comcast.net>
2015-03-18 10:38:28 -0400

    changed to i<=100, otherwise finished version of initial fizz-buzz app

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  for (var i = 1; i < 101; i++) {
+  for (var i = 1; i <= 100; i++) {
     if(i % 3 == 0 && i % 5 == 0) {
       $("body").append('<p>FizzBuzz</p>');
     }
