8e5623aca87549629fa8616da911da8b0f8f26d9
pkmoney1138 <pkmoney@comcast.net>
2015-03-18 10:33:46 -0400

    finished initial fizz-buzz app

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+  for (var i = 1; i < 101; i++) {
+    if(i % 3 == 0 && i % 5 == 0) {
+      $("body").append('<p>FizzBuzz</p>');
+    }
+    else if(i % 3 == 0) {
+      $('body').append('<p>Fizz</p>');
+    }
+    else if(i % 5 == 0) {
+      $('body').append('<p>Buzz</p>');
+    }
+    else {
+      $('body').append('<p>' + i + '</p>');
+    }
+  }
+})//final close
